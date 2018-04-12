a8799be5a602958c6bb8d46cf6d1113c49a96cc9
lx <1094290855@qq.com>
2018-04-12 10:55:03 +0800

    优化图片缩放比例

--- app/src/main/java/com/example/administrator/newsdf/photopicker/widget/TouchImageView.java
+++ app/src/main/java/com/example/administrator/newsdf/photopicker/widget/TouchImageView.java
@@ -126,7 +126,7 @@ public class TouchImageView extends android.support.v7.widget.AppCompatImageView
             mScaleType = ScaleType.FIT_CENTER;
         }
         minScale = 1;
-        maxScale = 3;
+        maxScale = 8;
         superMinScale = SUPER_MIN_MULTIPLIER * minScale;
         superMaxScale = SUPER_MAX_MULTIPLIER * maxScale;
         setImageMatrix(matrix);
