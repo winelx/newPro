8cde551f0eb3e32465d13bb2413e7e19ee57397c
winelx <1094290855@qq.com>
2021-02-05 11:04:49 +0800

    优化专项施工方案界面显示一半的问题

--- baselibrary/src/main/java/com/example/baselibrary/utils/Requests.java
+++ baselibrary/src/main/java/com/example/baselibrary/utils/Requests.java
@@ -8,9 +8,12 @@ package com.example.baselibrary.utils;
 public class Requests {
     // public static final String networks = "http://192.168.20.38:8082/pzgc/";
     //    public static final String networks = "http://192.168.20.120:8086/pzgc/";
-    // public static final String networks = "http://139.9.234.213:8085/pzgc/";
+    //测试
+    public static final String networks = "http://192.168.20.120:8087/pzgc/";
+    //
     //  public static final String networks = "http://192.168.20.81:8080/pzgc/";
-    public static final String networks = "http://120.79.142.15/pzgc/";
+    //正式
+    //  public static final String networks = "http://120.79.142.15/pzgc/";
     /**
      * VirtualBox
      * 登录
